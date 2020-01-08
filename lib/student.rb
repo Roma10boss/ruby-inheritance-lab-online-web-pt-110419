dec85535e44dad992f523daa516e833531d786df
Romario Gustave <romagustave10@gmail.com>
2020-01-08 00:09:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,8 @@ class Student < User
     @knowledge = []
   end 
   
-  def learn(knowledge)
+  def learn(string)
     @knowledge << string
   end 
-    def knowledge 
-      @knowledge
-    end 
+   
 end
