9adabca35889707cca10593e35bb5fb23c9a5567
Romario Gustave <romagustave10@gmail.com>
2020-01-08 00:04:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student < User
+class Student < User 
+  attr_accessor :knowledge 
+  
+  def initialize(knowledge)
+    @knowledge = []
+  end 
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+    
+    def knowledge 
+      @knowledge
+    end 
 
 end
